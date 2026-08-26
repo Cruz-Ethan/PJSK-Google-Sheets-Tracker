0cb1cd146fbd9b0438a4bdb64ef23e97f789533c
Ethan's GitHub <e.cruz1010047@gmail.com>
2026-08-25 19:04:57 -0700

    updated team stats to represent leader's skill being activated twice

--- utils/teamInfo.js
+++ utils/teamInfo.js
@@ -12,7 +12,7 @@ export function getTeamTalent(team) {
 
 export function getTeamHealthBoost(team) {
     let totalHealthBoost = 0
-    if(team.leader) totalHealthBoost += getSkillEffect(team.leader).healthBoost
+    if(team.leader) totalHealthBoost += getSkillEffect(team.leader).healthBoost * 2
     if(team.subleader) totalHealthBoost += getSkillEffect(team.subleader).healthBoost
     if(team.member1) totalHealthBoost += getSkillEffect(team.member1).healthBoost
     if(team.member2) totalHealthBoost += getSkillEffect(team.member2).healthBoost
@@ -22,7 +22,7 @@ export function getTeamHealthBoost(team) {
 
 export function getTeamScoreBoost(team) {
     let teamScoreBoost = 0
-    if(team.leader) teamScoreBoost += getSkillEffect(team.leader).scoreBoostPercentage
+    if(team.leader) teamScoreBoost += getSkillEffect(team.leader).scoreBoostPercentage * 2
     if(team.subleader) teamScoreBoost += getSkillEffect(team.subleader).scoreBoostPercentage
     if(team.member1) teamScoreBoost += getSkillEffect(team.member1).scoreBoostPercentage
     if(team.member2) teamScoreBoost += getSkillEffect(team.member2).scoreBoostPercentage
